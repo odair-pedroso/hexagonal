693cbed1317c16970cc99c2f22b1902c82da6396
odair-pedroso <odairpedroso90@gmail.com>
2023-09-21 18:52:29 -0300

    feat: create end points

---
+++ src/main/java/com/pedroso/hexagonal/application/core/usecase/DeleteCustomerByIdUseCase.java
@@ -0,0 +1,24 @@
+package com.pedroso.hexagonal.application.core.usecase;
+
+import com.pedroso.hexagonal.application.ports.in.FindCustomerByIdInputPort;
+import com.pedroso.hexagonal.application.ports.out.DeleteCustomerByIdOutputPort;
+
+public class DeleteCustomerByIdUseCase {
+
+    private final FindCustomerByIdInputPort findCustomerByIdInputPort;
+
+    private final DeleteCustomerByIdOutputPort deleteCustomerByIdOutputPort;
+
+    public DeleteCustomerByIdUseCase(FindCustomerByIdInputPort findCustomerByIdInputPort,
+                                     DeleteCustomerByIdOutputPort deleteCustomerByIdOutputPort) {
+        this.findCustomerByIdInputPort = findCustomerByIdInputPort;
+        this.deleteCustomerByIdOutputPort = deleteCustomerByIdOutputPort;
+
+    }
+
+    public void delete(String id) {
+        findCustomerByIdInputPort.find(id);
+        deleteCustomerByIdOutputPort.delete(id);
+
+    }
+}
---
+++ src/main/java/com/pedroso/hexagonal/application/ports/out/DeleteCustomerByIdOutputPort.java
@@ -0,0 +1,6 @@
+package com.pedroso.hexagonal.application.ports.out;
+
+public interface DeleteCustomerByIdOutputPort {
+
+    void delete(String id);
+}
