ab7e83cede6d4271cb60409255ac2b3625981c15
odair-pedroso <odairpedroso90@gmail.com>
2023-09-22 14:45:19 -0300

    feat: create producer for send cpf kafka

---
+++ src/main/java/com/pedroso/hexagonal/adapters/in/consumer/ReceiveValidatedCpfConsumer.java
@@ -0,0 +1,26 @@
+package com.pedroso.hexagonal.adapters.in.consumer;
+
+import com.pedroso.hexagonal.adapters.in.consumer.mapper.CustomerMessageMapper;
+import com.pedroso.hexagonal.adapters.in.consumer.message.CustomerMessage;
+import com.pedroso.hexagonal.application.ports.in.UpdateCustomerInputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReceiveValidatedCpfConsumer {
+
+    @Autowired
+    private UpdateCustomerInputPort updateCustomerInputPort;
+
+    @Autowired
+    private CustomerMessageMapper customerMessageMapper;
+
+
+    @KafkaListener(topics = "tp-cpf-validated", groupId = "pedroso")
+    public void receive(CustomerMessage customerMessage) {
+        var customer = customerMessageMapper.toCustomer(customerMessage);
+        updateCustomerInputPort.update(customer, customerMessage.getZipCode());
+
+    }
+}
